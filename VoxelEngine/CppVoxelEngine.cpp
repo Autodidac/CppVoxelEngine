409688a026a55c64d3dac050b7cbe5fde8ae8c16
Autodidac <177971875+Autodidac@users.noreply.github.com>
2024-08-27 00:18:30 -0400

    Create CppVoxelEngine.cpp

---
+++ VoxelEngine/CppVoxelEngine.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+// CppVoxelEngine.cpp
+#include "CppVoxelEngine.h"
+#include "GreedyMesher.h"
+#include "Chunk.h"
+#include <iostream>
+
+void InitializeLibrary() {
+    std::cout << "Library Initialized" << std::endl;
+}
+
+void CleanupLibrary() {
+    std::cout << "Library Cleaned Up" << std::endl;
+}
+
+void generateTerrain(int someParameter) {
+    // Implementation of generateTerrain
+}
